9be147b8ac99beb1522b4dd78ce61ee388f13f69
nitishkumar149345 <nitishmech149@gmail.com>
2024-05-08 11:05:40 +0530

    vendor

--- vendors/views.py
+++ vendors/views.py
@@ -15,8 +15,8 @@ def formatresponse( message, data=None):
 
 
 def get_tokens_for_user(user):
-  refresh = RefreshToken.for_user(user)
-  return {
+    refresh = RefreshToken.for_user(user)
+    return {
       'refresh': str(refresh),
       'access': str(refresh.access_token),
   }
